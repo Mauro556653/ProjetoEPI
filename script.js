de798b7e380767e12b8babf81cf2cb4b0fbf9b5b
Mauro556653 <mauroignacio.bvb2017@gmail.com>
2025-05-28 18:51:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function login() {
-    const usuario = document.getElementById("usuario").value;
-    const senha = document.getElementById("senha").value;
-    const mensagem = document.getElementById("mensagem");
+  const usuario = document.getElementById('usuario').value;
+  const senha = document.getElementById('senha').value;
+  const erro = document.getElementById('erro');
 
-    if (usuario === "admin" && senha === "123") {
-        window.location.href = "formulario.html";
-    } else {
-        mensagem.style.color = "yellow";
-        mensagem.textContent = "Usuário ou senha incorretos.";
-    }
+  if (usuario === '08073154943' && senha === 'admin123') {
+    window.location.href = 'dashboard.html'; // Aqui irá para o painel do admin
+  } else {
+    erro.textContent = 'Usuário ou senha incorretos.';
+  }
 }
